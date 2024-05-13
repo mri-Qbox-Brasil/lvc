4593cd305fe61b4e5c2f445669a3d13afbeb9e2f
kaJseia <89431075+mur4i@users.noreply.github.com>
2024-05-13 18:13:47 -0300

    Clear fxmanifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -75,12 +75,12 @@ client_scripts {
 	'/UTIL/cl_*.lua',
 	'/UI/cl_*.lua',
 	'/PLUGINS/cl_plugins.lua',
-	'/PLUGINS/**/SETTINGS.lua',
-	'/PLUGINS/**/cl_*.lua',
+	-- '/PLUGINS/**/SETTINGS.lua',
+	-- '/PLUGINS/**/cl_*.lua',
 }
 
 server_script {
 	'/UTIL/sv_lvc.lua',
-	'/PLUGINS/**/sv_*.lua'
+	-- '/PLUGINS/**/sv_*.lua'
 }
 ------------------------------
